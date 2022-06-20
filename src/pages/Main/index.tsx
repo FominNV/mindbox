0db77eb4620e2fa06d2ff1a19ef4234c36356227
Nikolai Fomin <kk309@mail.ru>
2022-06-20 11:50:09 +0300

    create Main page

---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import Desk from "components/Desk";
+
+import "./styles.scss";
+
+const Main: FC = () => (
+  <div className="Main">
+    <Desk />
+  </div>
+);
+
+export default Main;

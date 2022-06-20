a383e4dfa1c4b865806efade29436d72047b7c6e
Nikolai Fomin <kk309@mail.ru>
2022-06-20 11:46:43 +0300

    create and config project

---
+++ README.md
@@ -0,0 +1,11 @@
+В данном проекте используется следующий стек:
+
+Билиотеки - React.js, Typescript, Redux;
+
+Стилизация - Sass, flexbox;
+
+Код - Eslint, Prettier;
+
+Оптимизация изображений - Adobe Photoshop
+
+Deploy #Mindbox
---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
+
+import { store } from "./store";
+
+const rootElement = document.getElementById("root") || document.createElement("div") as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
